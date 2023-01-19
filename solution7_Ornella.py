3b06b4e34c46f3a755bddda3a71d77259ad9d7e2
boris-ny <b.nyilindek@alustudent.com>
2023-01-19 15:49:42 +0200

    commit by Ornella

--- solution7_Ornella.py
+++ solution7_Ornella.py
@@ -1,13 +1,14 @@
 #The hex color codes should match the pattern "#XXXXXX" where X is any letter or digit.
+
 import re
 
-user_name_pattern= re.compile(r'#(.{})')
+def is_hex_color(color):
+    pattern = re.compile("^#[0-9A-Fa-f]{6}$")
+    return pattern.match(color)
 
-user_name = input("Enter a twitter handle: ")
-
-match = re.search(user_name_pattern, user_name)
-
-if match:
-    print(user_name)
-else:
-    print("Invalid twitter handle")
+print(is_hex_color("#ff0000")) # True
+print(is_hex_color("#abcdef")) # True
+print(is_hex_color("#123456")) # True
+print(is_hex_color("#GGGHHH")) # False (invalid characters)
+print(is_hex_color("#12345"))  # False (too short)
+print(is_hex_color("#1234567")) # False (too long)
