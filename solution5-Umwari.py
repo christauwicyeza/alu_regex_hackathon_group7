eb8165d0f0418f95c5a7f330799f8d05fe66fc58
M-Umwari <m.umwari@alustudent.com>
2023-01-19 16:48:50 +0200

    jokes

--- solution5-Umwari.py
+++ solution5-Umwari.py
@@ -1,13 +1,14 @@
 import re
 
 def is_valid_joke(joke):
-    match =re.match(r"why did the.*? \? Because.*" ,joke)
+    match = re.match(r"Why did the .? \? Because.", joke)
     if match:
         return True
     else:
         return False
 
-print(is_valid_joke("Why did the chicken cross the road? To get to the other side.")) 
-print(is_valid_joke("Why did the Tomato turn red? Because it saw the salad dressing."))
-print(is_valid_joke("why did the math book look sad? Because it had too many problems."))
-print(is_valid_joke("why did the cookie got to the doctor? Because it was feeling crumbly. "))
+print(is_valid_joke("Why did the chicken cross the road? To get to the other side.")) #True
+print(is_valid_joke("Why did the tomato turn red? Because it saw the salad dressing.")) #True
+print(is_valid_joke("Why did the math book look sad? Because it had too many problems.")) #True
+print(is_valid_joke("Why did the cookie go to the doctor? Because it was feeling crumbly.")) #True
+
